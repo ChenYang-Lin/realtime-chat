a3279656c5274e6a37d2e84bc6ca1922441b5398
ChenYang <chenyanglinq@gmail.com>
2020-04-24 17:57:22 -0400

    init

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var socket= require('socket.io');
+
+//app setup
+var app = express();
+var server = app.listen(3000, function(){
+    console.log("listen to requests on port 3000");
+})
+
+//static files (middlewares)
+app.use(express.static('public'));
+
+
+//Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('made socket connection', socket.id);
+
+    //handle chat event
+    socket.on('chat', function(data){
+        io.emit('chat', data);
+    })
+
+    //broadcasting
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    })
+})
