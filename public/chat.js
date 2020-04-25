647a3184787c7ad50fbc93a6ed40427b9bd886ea
ChenYang <chenyanglinq@gmail.com>
2020-04-25 10:02:07 -0400

    socket sd

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 //make connection
-var socket = io.connect('http://chat-app-yangge-76501.herokuapp.com');
+var socket = io.connect('https://chat-app-yangge.herokuapp.com');
 //var socket = io.connect('http://localhost:3000');
 //var socket = socketIO(server);
 
